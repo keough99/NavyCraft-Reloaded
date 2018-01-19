564cc66a0bfa2a3a3d98fdb416d2074ce920b646
Solmex72 <solmexgaming@gmail.com>
2018-01-19 05:43:02 -0500

    Purge Comments 1
    Removed unnecessary comments in AimCannon

--- src/com/maximuspayne/aimcannon/AimCannonPlayerListener.java
+++ src/com/maximuspayne/aimcannon/AimCannonPlayerListener.java
@@ -24,7 +24,6 @@ public class AimCannonPlayerListener implements Listener {
     ///////stone button
 	if (event.getAction() == Action.LEFT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_BLOCK ) {
 	    if (event.getClickedBlock().getType() == Material.STONE_BUTTON) {
-		// System.out.print("Button clicked");
 		Block b = null;
 		if (event.getClickedBlock().getRelative(BlockFace.NORTH_EAST).getType() == Material.DISPENSER) {
 		    b = event.getClickedBlock().getRelative(BlockFace.NORTH_EAST);
@@ -63,7 +62,6 @@ public class AimCannonPlayerListener implements Listener {
 					    if (onec.isCharged() ) {
 						onec.Action(event.getPlayer());
 					    } else {
-						// System.out.print("Load Cannon first");
 						event.getPlayer().sendMessage("Load Cannon first.. (left click Dispenser)");
 					    }
 					}else
@@ -109,7 +107,6 @@ public class AimCannonPlayerListener implements Listener {
 	    //////Levers
 	    if (event.getAction() == Action.LEFT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_BLOCK ) {
 		    if (event.getClickedBlock().getType() == Material.LEVER) {
-			// System.out.print("Lever used");
 			Block b = null;
 			
 			////cannon levers
@@ -296,13 +293,11 @@ public class AimCannonPlayerListener implements Listener {
 		// new Cannon
 		OneCannon oc = new OneCannon(event.getClickedBlock().getLocation(), NavyCraft.instance);
 		if (oc.isValidCannon(event.getClickedBlock())) {
-		    // System.out.print("New Cannon");
 			if( event.getAction() == Action.LEFT_CLICK_BLOCK )
 				oc.Charge(event.getPlayer(), true);
 			else
 				oc.Charge(event.getPlayer(), false);
 		    AimCannon.cannons.add(oc);
-		    // event.getPlayer().sendMessage("Cannon charged..");
 		}
 	    }
 	    
@@ -315,13 +310,10 @@ public class AimCannonPlayerListener implements Listener {
 	public static void onBlockDispense(BlockDispenseEvent event) {
     	if( event.getBlock() != null && event.getBlock().getTypeId() == 23 )
     	{
-    		//System.out.println("test1");
 	    	for (OneCannon onec : AimCannon.getCannons()) 
 			{
-	    		//System.out.println("test2");
 			    if (onec.isThisCannon(event.getBlock().getLocation(), true)) 
 			    {
-			    	//System.out.println("test3");
 			    	event.setCancelled(true);
 			    	return;
 			    }
