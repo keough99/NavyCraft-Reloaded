c3cbc23fbeca6b6aaac5025863b14849de1071a0
Solmex <solmexgaming@gmail.com>
2018-01-29 21:22:16 -0500

    Ship Freezing
    Fixed all of the ship freezing!
    yay.

--- src/com/maximuspayne/navycraft/craft/Craft.java
+++ src/com/maximuspayne/navycraft/craft/Craft.java
@@ -488,7 +488,7 @@ public class Craft {
 		
 		// all craft types can move through air and flowing water/lava
 		if ( blockId == 0 ||
-				(blockId >= 8 && blockId <= 11) ||
+				(blockId >= 8 && blockId <= 11 && data != 0) ||
 				//blockId == 78 || 
 				BlocksInfo.coversGrass(blockId)) //snow cover
 			return true;
