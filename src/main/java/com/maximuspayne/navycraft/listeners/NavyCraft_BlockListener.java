ecd8d1bc4dd488fa17e148ad364fc77f00e34749
Connor Woods <solmexgaming@gmail.com>
2018-08-16 09:39:23 -0400

    trying to fix shipyard signs

--- src/main/java/com/maximuspayne/navycraft/listeners/NavyCraft_BlockListener.java
+++ src/main/java/com/maximuspayne/navycraft/listeners/NavyCraft_BlockListener.java
@@ -444,7 +444,7 @@ public class NavyCraft_BlockListener implements Listener {
 						int y = sign.getY();
 						int z = sign.getZ();
 						World world = sign.getWorld();
-						NavyCraft_FileListener.saveClaimedSign(player.getName(), lotStr, world.getName(), x, y, z, maxId(player) + 1);
+						NavyCraft_FileListener.saveClaimedSign(UUID, lotStr, world.getName(), x, y, z, maxId(player) + 1);
 						
 						NavyCraft_FileListener.loadSignData();
 					} else {
@@ -2054,8 +2054,11 @@ public class NavyCraft_BlockListener implements Listener {
 		int foundHighest = -1;
 		String UUID = PermissionInterface.getUUIDfromPlayer(player.getName());
 		if (UUID != null) {
+		NavyCraft.instance.DebugMessage("UUID check passed", 3);
 		if (NavyCraft.playerSigns.containsKey(UUID)) {
+			NavyCraft.instance.DebugMessage("Player signs contained player", 3);
 			for (Plot p : NavyCraft.playerSigns.get(UUID)) {
+				NavyCraft.instance.DebugMessage("Checking player plot:" + p.name, 3);
 				if (foundHighest < NavyCraft.playerSignIndex.get(p.sign)) {
 					foundHighest = NavyCraft.playerSignIndex.get(p.sign);
 					NavyCraft.instance.DebugMessage(String.valueOf(foundHighest), 3);
--- src/main/java/com/maximuspayne/navycraft/listeners/NavyCraft_FileListener.java
+++ src/main/java/com/maximuspayne/navycraft/listeners/NavyCraft_FileListener.java
@@ -80,57 +80,74 @@ public class NavyCraft_FileListener implements Listener {
 		return selectSignBlock;
 	}
 
-	public static void saveClaimedSign(String player, String type, String world, int x, int y, int z, int id) {
+	public static void saveClaimedSign(String uuid, String type, String world, int x, int y, int z, int id) {
+		boolean isRegistered = false;
+		String num = null;
 		Block selectSignBlock = NavyCraft.instance.getServer().getWorld(world).getBlockAt(x, y, z);
 		if (selectSignBlock.getTypeId() == 63) {
 		Location loc = new Location(NavyCraft.instance.getServer().getWorld(world), x, y, z);
-		String UUID = PermissionInterface.getUUIDfromPlayer(player);
 		List<String> list = new ArrayList<String>(ConfigManager.syData.getConfigurationSection("Signs").getKeys(false));
-		for (String num : list) {
+		for (String num1 : list) {
 			int x1 = ConfigManager.syData.getInt("Signs." + num + "." + "x");
 			int y1 = ConfigManager.syData.getInt("Signs." + num + "." + "y");
 			int z1 = ConfigManager.syData.getInt("Signs." + num + "." + "z");
 			String world1 = ConfigManager.syData.getString("Signs." + num + "." + "world");
 			Location loc1 = new Location(NavyCraft.instance.getServer().getWorld(world1), x1, y1, z1);
 			if (loc.equals(loc1)) {
-				ConfigManager.syData.set("Signs." + num + "." + "type", type.toUpperCase());
-				ConfigManager.syData.set("Signs." + num + "." + "world", world);
-				ConfigManager.syData.set("Signs." + num + "." + "x", x);
-				ConfigManager.syData.set("Signs." + num + "." + "y", y);
-				ConfigManager.syData.set("Signs." + num + "." + "z", z);
-				ConfigManager.syData.set("Signs." + num + "." + "isClaimed", true);
-				ConfigManager.syData.set("Signs." + num + "." + "uuid", UUID);
-				ConfigManager.syData.set("Signs." + num + "." + "id", id);
+				num = num1;
+				isRegistered = true;
 				break;
+			} else {
+				isRegistered = false;
 			}
 		}
+		if (!isRegistered) {
+			num = String.valueOf(list.size() + 1);
+		}
+		ConfigManager.syData.set("Signs." + num + "." + "type", type.toUpperCase());
+		ConfigManager.syData.set("Signs." + num + "." + "world", world);
+		ConfigManager.syData.set("Signs." + num + "." + "x", x);
+		ConfigManager.syData.set("Signs." + num + "." + "y", y);
+		ConfigManager.syData.set("Signs." + num + "." + "z", z);
+		ConfigManager.syData.set("Signs." + num + "." + "isClaimed", true);
+		ConfigManager.syData.set("Signs." + num + "." + "uuid", uuid);
+		ConfigManager.syData.set("Signs." + num + "." + "id", id);
 		ConfigManager.savesyData();
 	}
 }
 	
 	public static void saveUnclaimedSign(String type, String world, int x, int y, int z) {
+		boolean isRegistered = false;
+		String num = null;
 		Block selectSignBlock = NavyCraft.instance.getServer().getWorld(world).getBlockAt(x, y, z);
 		if (selectSignBlock.getTypeId() == 63) {
 		Location loc = new Location(NavyCraft.instance.getServer().getWorld(world), x, y, z);
 		List<String> list = new ArrayList<String>(ConfigManager.syData.getConfigurationSection("Signs").getKeys(false));
-		for (String num : list) {
+		for (String num1 : list) {
 			int x1 = ConfigManager.syData.getInt("Signs." + num + "." + "x");
 			int y1 = ConfigManager.syData.getInt("Signs." + num + "." + "y");
 			int z1 = ConfigManager.syData.getInt("Signs." + num + "." + "z");
 			String world1 = ConfigManager.syData.getString("Signs." + num + "." + "world");
 			Location loc1 = new Location(NavyCraft.instance.getServer().getWorld(world1), x1, y1, z1);
 			if (loc.equals(loc1)) {
-				ConfigManager.syData.set("Signs." + num + "." + "type", type.toUpperCase());
-				ConfigManager.syData.set("Signs." + num + "." + "world", world);
-				ConfigManager.syData.set("Signs." + num + "." + "x", x);
-				ConfigManager.syData.set("Signs." + num + "." + "y", y);
-				ConfigManager.syData.set("Signs." + num + "." + "z", z);
-				ConfigManager.syData.set("Signs." + num + "." + "isClaimed", false);
-				ConfigManager.syData.set("Signs." + num + "." + "uuid", null);
-				ConfigManager.syData.set("Signs." + num + "." + "id", null);
+				num = num1;
+				isRegistered = true;
 				break;
+			} else {
+				isRegistered = false;
 			}
 		}
+		if (!isRegistered) {
+			num = String.valueOf(list.size() + 1);
+		}
+		ConfigManager.syData.set("Signs." + num + "." + "type", type.toUpperCase());
+		ConfigManager.syData.set("Signs." + num + "." + "world", world);
+		ConfigManager.syData.set("Signs." + num + "." + "x", x);
+		ConfigManager.syData.set("Signs." + num + "." + "y", y);
+		ConfigManager.syData.set("Signs." + num + "." + "z", z);
+		ConfigManager.syData.set("Signs." + num + "." + "isClaimed", false);
+		ConfigManager.syData.set("Signs." + num + "." + "uuid", null);
+		ConfigManager.syData.set("Signs." + num + "." + "id", null);
 		ConfigManager.savesyData();
 	}
 }
